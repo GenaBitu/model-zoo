2e5069349c4e2b8aea938a26cdc1fbdf574348ca
GenaBitu <genabitu@gmail.com>
2018-03-08 17:07:41 +0100

    Using matrix datasets

--- twomoon/dataset.jl
+++ twomoon/dataset.jl
@@ -1,24 +1,25 @@
 using Plots;
 
-function generateTwoMoonDS(count; T::Type = Float32, std = 0.025f0)::AbstractVector
-	ds = Vector{Tuple{Vector, Int}}(count);
-	for i in 1:length(ds)
-		label = rand(0:1);
-		if label == 0
+function generateTwoMoonDS(count::Int; T::Type = Float32, std = 0.025f0)::Tuple{AbstractMatrix, AbstractVector}
+	X = Matrix{T}(2, count);
+	Y = Vector{Int}(count);
+	for i in 1:count
+		Y[i] = rand(1:2);
+		if Y[i] == 1
 			x = 0.3f0 + rand(T) * 0.6f0;
-			feature = [x, -2*sqrt(0.3^2 - (x - 0.6)^2) + 0.7] .+ (randn(2) .* std);
+			X[:, i] = [x, -2*sqrt(0.3^2 - (x - 0.6)^2) + 0.7] .+ (randn(2) .* std);
 		else
 			x = 0.1f0 + rand(T) * 0.6f0;
-			feature = [x, 2*sqrt(0.3^2 - (x - 0.4)^2) + 0.3] .+ (randn(2) .* std);
+			X[:, i] = [x, 2*sqrt(0.3^2 - (x - 0.4)^2) + 0.3] .+ (randn(2) .* std);
 		end
-		ds[i] = (feature, label)
 	end
-	return ds;
+	return (X, Y);
 end
 
-function plotTwoMoonDS(ds::AbstractVector)
-	negds = getindex.(ds[getindex.(ds, 2) .== 0], 1);
-	posds = getindex.(ds[getindex.(ds, 2) .== 1], 1);
-	scatter(getindex.(negds, 1), getindex.(negds, 2), label = "negative", aspect_ratio = :equal, xlims=(0:1), ylims=(0:1));
-	scatter!(getindex.(posds, 1), getindex.(posds, 2), label = "positive");
+function plotTwoMoonDS(ds::Tuple{AbstractMatrix, AbstractVector})
+	(X, Y) = ds
+	negds = X[:, Y .== 1];
+	posds = X[:, Y .== 2];
+	scatter(getindex(negds, 1, :), getindex(negds, 2, :), label = "negative", aspect_ratio = :equal, xlims=(0:1), ylims=(0:1));
+	scatter!(getindex(posds, 1, :), getindex(posds, 2, :), label = "positive");
 end
--- twomoon/model.jl
+++ twomoon/model.jl
@@ -2,21 +2,21 @@ using Flux;
 
 include("dataset.jl")
 
-Layer(in::Int, out::Int) = Flux.Dense(in, out, Flux.relu);
+Layer(in::Int, out::Int) = Flux.Dense(in, out, tanh);
 
-model = Chain(Layer(2, 8), Layer(8, 8), Layer(8, 8), Dense(8, 2), softmax);
+model = Chain(Layer(2, 4), Layer(4, 8), Layer(8, 8), Layer(8, 4), Dense(4, 2), softmax);
 trainDS = generateTwoMoonDS(1000);
 testDS = generateTwoMoonDS(100);
 
-loss(x, y) = Flux.crossentropy(model(x), [y, 1-y]);
+loss(x, y) = Flux.logitcrossentropy(model(x), Flux.onehotbatch(y, 1:2));
 
-for _ in 1:100
-	Flux.train!(loss, trainDS, ADAM(params(model)));
+for _ in 1:10000
+	Flux.train!(loss, [trainDS], ADAM(params(model)));
 end
 
 x = 0:0.01:1;
 y = 0:0.01:1;
-z = [model([xi, yi])[1].data[1] for (yi, xi) in Base.product(x, y)];
+z = [model([yi, xi]).data[2] for (xi, yi) in Base.product(x, y)];
 
 plotTwoMoonDS(testDS);
 heatmap!(x, y, z);
