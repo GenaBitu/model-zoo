74d33062a23924c31f522f6e6782a00e806704c6
GenaBitu <genabitu@gmail.com>
2018-04-05 17:27:34 +0200

    Added plotModel function

--- twomoon/model.jl
+++ twomoon/model.jl
@@ -2,6 +2,11 @@ using Flux;
 
 include("dataset.jl")
 
+function plotModel(ds::Tuple{AbstractMatrix, AbstractVector}, model)
+	plotTwoMoonDS(testDS);
+	heatmap!(x, y, z);
+end
+
 Layer(in::Int, out::Int) = Flux.Dense(in, out, tanh);
 
 model = Chain(Layer(2, 4), Layer(4, 8), Layer(8, 8), Layer(8, 4), Dense(4, 2), softmax);
@@ -18,5 +23,4 @@ x = 0:0.01:1;
 y = 0:0.01:1;
 z = [model([yi, xi]).data[2] for (xi, yi) in Base.product(x, y)];
 
-plotTwoMoonDS(testDS);
-heatmap!(x, y, z);
+plotModel(testDS, model);
