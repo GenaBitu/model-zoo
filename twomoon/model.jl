9a785d7948c2306701fbfd28d0bb0d7c019d917d
GenaBitu <genabitu@gmail.com>
2018-06-06 16:12:51 +0200

    Flux update

--- twomoon/model.jl
+++ twomoon/model.jl
@@ -10,27 +10,28 @@ function plotModel(ds::Tuple{AbstractMatrix, Flux.OneHotMatrix}, model, performa
 	plot(p1, p2, layout=(1,2));
 end
 
-numBatches = 1000;
-σ = Flux.relu;
+numBatches = 2000;
+σ = Flux.swish;
+noiseDeviation = 0.2;
 loss = Flux.mse;
-modelloss(x, y) = Flux.mse(softmax(x), y);
+modelloss(x, y) = Flux.mse(x, y);
 #reg = Flux.l2(0.000001);
 #reg = Flux.regcov(0.00001);
 reg = Flux.l2(0);
 
 #Layer(in::Int, out::Int) = Target(Chain(Dense(in, 8, σ), Dense(8, 8, σ), Dense(8, out)), Chain(Dense(out, 8, σ), Dense(8, 8, σ), Dense(8, in, identity)), loss; regulariser = reg);
-Layer(in::Int, out::Int) = Target(Chain(Dense(in, out, σ)), Chain(Dense(out, 8, σ), Dense(8, in, identity)), loss; regulariser = reg);
+Layer(in::Int, out::Int) = Target(Chain(Dense(in, out, σ)), Chain(Dense(out, 8, σ), Dense(8, in, identity)), loss; σ = noiseDeviation, regulariser = reg);
 
 #model = Chain(Layer(2, 4), Layer(4, 8), Layer(8, 8), Layer(8, 4), Target(4, 2, tanh, loss; regulariser = reg));
 #model = Chain(Layer(2, 16), Layer(16, 2));
-model = Chain(Layer(2, 2));
-trainDS = generateTwoMoonDS(10);
+model = Chain(Layer(2, 16), Layer(16, 2), Target(softmax, Chain(Dense(2, 4, σ), Dense(4, 2, identity)), loss; σ = noiseDeviation));
+trainDS = generateTwoMoonDS(1000);
 testDS = generateTwoMoonDS(100);
 
 performance = Vector{Float32}(numBatches);
 
 for i in 1:numBatches
-	targettrain!(model, modelloss, [trainDS], ADAM(params(model)), η = 0.01, cb = () -> begin performance[i] = Flux.data(modelloss(model(testDS[1]), testDS[2])); end);
+	targettrain!(model, modelloss, [trainDS], ADAM(params(model)), η = 0.5, cb = () -> begin performance[i] = Flux.data(modelloss(model(testDS[1]), testDS[2])); end);
 end
 
 x = 0:0.01:1;
