6d91476e0b9fc00796f9a722e7de32d16afb002e
GenaBitu <genabitu@gmail.com>
2018-08-29 22:07:42 +0200

    Refactor

--- other/twomoon/model.jl
+++ other/twomoon/model.jl
@@ -5,6 +5,8 @@ include("dataset.jl")
 
 function plotModel(ds::Tuple{AbstractMatrix, Flux.OneHotMatrix}, model, performance)
 	plots = [plotTwoMoonDS(testDS)];
+	x = y = linspace(0, 1, 100);
+	z = [Flux.data(softmax(model([yi, xi])))[2] for (xi, yi) in Base.product(x, y)];
 	heatmap!(plots[1], x, y, z, colorbar = false, title = "Heatmap");
 	push!(plots, plot(performance, label = "Loss", ylims = (0.001, 1), yscale = :log10, title = "Final loss"));
 	return plots;
@@ -15,8 +17,8 @@ numBatches = 2000;
 modelloss(x, y) = Flux.mse(softmax(x), y);
 
 Layer(in::Int, out::Int) = Dense(in, out, σ);
-
 model = Chain(Layer(2, 16), Layer(16, 2));
+
 trainDS = generateTwoMoonDS(1000);
 testDS = generateTwoMoonDS(1000);
 
@@ -25,8 +27,5 @@ performance = Vector{Float32}(numBatches);
 for i in 1:numBatches
 	Flux.train!((x, y)->modelloss(model(x), y), [trainDS], ADAM(params(model)), cb = () -> begin performance[i] = Flux.data(modelloss(model(testDS[1]), testDS[2])); end);
 end
-
-x = y = linspace(0, 1, 100);
-z = [Flux.data(softmax(model([yi, xi])))[2] for (xi, yi) in Base.product(x, y)];
 
 plotModel(testDS, model, performance);
