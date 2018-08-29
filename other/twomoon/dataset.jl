77061f8d1a13929905828cf683bae718967aa5ac
GenaBitu <genabitu@gmail.com>
2018-08-29 15:37:27 +0200

    Synced dataset updates

--- other/twomoon/dataset.jl
+++ other/twomoon/dataset.jl
@@ -1,6 +1,7 @@
+using Flux;
 using Plots;
 
-function generateTwoMoonDS(count::Int; T::Type = Float32, std = 0.025f0)::Tuple{AbstractMatrix, AbstractVector}
+function generateTwoMoonDS(count::Int; T::Type = Float32, std = 0.025f0)::Tuple{AbstractMatrix, Flux.OneHotMatrix}
 	X = Matrix{T}(2, count);
 	Y = Vector{Int}(count);
 	for i in 1:count
@@ -10,16 +11,17 @@ function generateTwoMoonDS(count::Int; T::Type = Float32, std = 0.025f0)::Tuple{
 			X[:, i] = [x, -2*sqrt(0.3^2 - (x - 0.6)^2) + 0.7] .+ (randn(2) .* std);
 		else
 			x = 0.1f0 + rand(T) * 0.6f0;
-			X[:, i] = [x, 2*sqrt(0.3^2 - (x - 0.4)^2) + 0.3] .+ (randn(2) .* std);
+			X[:, i] = [x, +2*sqrt(0.3^2 - (x - 0.4)^2) + 0.3] .+ (randn(2) .* std);
 		end
 	end
-	return (X, Y);
+	return (X, Flux.onehotbatch(Y, 1:2));
 end
 
-function plotTwoMoonDS(ds::Tuple{AbstractMatrix, AbstractVector})
-	(X, Y) = ds
-	negds = X[:, Y .== 1];
-	posds = X[:, Y .== 2];
+function plotTwoMoonDS(ds::Tuple{AbstractMatrix, Flux.OneHotMatrix})
+	(X, Y) = ds;
+	Y = Y[2, :];
+	negds = X[:, Y .== false];
+	posds = X[:, Y .== true];
 	res = scatter(getindex(negds, 1, :), getindex(negds, 2, :), label = "negative", aspect_ratio = :equal, xlims = (0:1), ylims = (0:1));
 	scatter!(res, getindex(posds, 1, :), getindex(posds, 2, :), label = "positive");
 	return res;
